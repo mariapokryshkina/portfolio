9689d5f08486199490c2b98c79ed0544e2d32bff
Maria <bc-macair023@BCmacair023sAir.wlan>
2022-02-14 11:25:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.querySelector(".main_nav").style.backgroundColor = "SandyBrown";
+   
+  } else {
+    document.querySelector(".main_nav").style.backgroundColor = "transparent";
+   
+  }
+}
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
+function myFunction() {
+  var x = document.getElementById("my_main_nav");
+  if (x.className === "main_nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "main_nav";
+  }
+}
